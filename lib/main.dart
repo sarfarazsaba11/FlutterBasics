7779377dc793a8c1d5370f6340cbc4ca1847171e
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-08-30 23:55:28 +0500

    update main application entry point to use ProductPage instead of ProfileScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myfirst_project/responsive_ui/partition.dart';
 import 'package:myfirst_project/assign/profilescreen.dart';
+import 'package:myfirst_project/responsive_ui/product_page.dart';
 import 'package:myfirst_project/screens/bottom_navigation_bar_screen.dart';
 import 'package:myfirst_project/screens/drawer.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home:ProfileScreen(),
+      home:ProductPage(),
       theme: ThemeData(),
     )
   );

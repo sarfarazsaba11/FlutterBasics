9cc86c446e20fbed3d6c6a06d4805fdac4bbf809
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-09-13 23:59:06 +0500

    add SettingScreen implementation with gradient background and settings icon

---
+++ lib/onepage/setting_screen.dart
@@ -0,0 +1,46 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return SettingScreenState();
+  }
+}
+
+class SettingScreenState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.red, Colors.purple],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+      ),
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 20, bottom: 20),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.settings, size: 88, color: Colors.white),
+            Text(
+              "This is my setting Screen",
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}

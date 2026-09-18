3a178a3bba0c81b32d005b2fd2526a48060ded85
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-09-18 23:28:40 +0500

    update main application entry point to use JsonScreen and add additional imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myfirst_project/assign/assign_2/login_screen.dart';
 import 'package:myfirst_project/responsive_ui/partition.dart';
 import 'package:myfirst_project/assign/profilescreen.dart';
 import 'package:myfirst_project/responsive_ui/product_page.dart';
 import 'package:myfirst_project/screens/bottom_navigation_bar_screen.dart';
 import 'package:myfirst_project/screens/drawer.dart';
 import 'package:myfirst_project/screens/form_validation.dart';
+import 'package:myfirst_project/screens/week7/gallery.dart';
+import 'package:myfirst_project/screens/week7/json_screen.dart';
+import 'package:myfirst_project/screens/week7/permission_screen.dart';
 import 'package:myfirst_project/statefulScreen/ListView_Screen.dart';
 import 'package:myfirst_project/statefulScreen/grid_page.dart';
 import 'package:myfirst_project/statefulScreen/input_screen.dart';
@@ -17,7 +21,7 @@ import 'onepage/parenttSceen.dart';
 void main() {
   runApp(
     MaterialApp(
-      home:ListPage(),
+      home:JsonScreen(),
       theme: ThemeData(),
     )
   );
